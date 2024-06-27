8be1711701702fdc5f3ec03fe75b28d861bc532d
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 13:38:44 +0300

    Initial commit

---
+++ SpecialNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿class SpecialNumber
+{
+    public static void Main(string[]args)
+    {
+
+        int number = int.Parse(Console.ReadLine());
+
+        int originalNum = number;
+        bool isSpecial = false;
+
+        while (originalNum > 0)
+        {
+            int digit = originalNum % 10;
+
+            if (number % digit == 0)
+            {
+                isSpecial = true;
+            }
+            else
+            {
+                isSpecial = false;
+                break;
+            }
+            originalNum /= 10;
+        }
+
+        if (isSpecial)
+        {
+                Console.WriteLine($"{number} is special"); 
+        }
+        else 
+        {
+                Console.WriteLine($"{number} is not special"); 
+        }
+    }
+
+    }
